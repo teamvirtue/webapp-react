7bbe7affecb679a5c5116830c2da39b2647687de
Jordy-A <jordyalblas@live.nl>
2018-04-03 12:25:18 +0200

    Controls page: Left navigation

--- src/views/controls/Controls.js
+++ src/views/controls/Controls.js
@@ -5,18 +5,9 @@ import { withStyles } from 'material-ui/styles';
 import RoomNavigation from './RoomNavigation';
 import SwipeableViews from 'react-swipeable-views';
 import Typography from 'material-ui/Typography';
-import 'rc-slider/assets/index.css';
-import List, {
-    ListItem,
-    ListItemIcon,
-    ListItemSecondaryAction,
-    ListItemText,
-} from 'material-ui/List';
 import AppBar from 'material-ui/AppBar';
+import List from 'material-ui/List';
 import Tabs, { Tab } from 'material-ui/Tabs';
-import Switch from 'material-ui/Switch';
-import Slider from 'rc-slider';
-import Icon from 'material-ui/Icon';
 import grey from 'material-ui/colors/grey';
 
 //import './Controls.css';
@@ -66,19 +57,6 @@ TabContainer.propTypes = {
 
 
 class Controls extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            checkedLightOnOff: false,
-            checkedNightMode: true,
-            checkedTemperature: true,
-        };
-    }
-	
-    handleChange = name => (event, checked) => {
-        this.setState({ [name]: checked });
-    };
-
     render() {
         const { classes, theme } = this.props;
 
@@ -91,160 +69,7 @@ class Controls extends Component {
                 <ImageCircle imageSource={ myLinqImage }/>
 
                 <div className='panelView'>
-                    <div className='row'>
-						<div className='col-xs-5'>
-							<RoomNavigation />
-						</div>
-						<div className='col-xs-7'>
-								
-							<List>
-								<Typography className={ classes.controlsTitle } type='subheading'>
-									Lights
-								</Typography>
-								
-								<ListItem>
-									<ListItemIcon>
-										<Icon>power_settings_new</Icon>
-									</ListItemIcon>
-
-									<ListItemText primary='Power' />
-
-									<ListItemSecondaryAction>
-										<Switch
-											checked={ this.state.checkedLightOnOff }
-											onChange={ this.handleChange('checkedLightOnOff') }
-										/>
-									</ListItemSecondaryAction>
-								</ListItem>
-								<ListItem>
-									<ListItemIcon>
-										<Icon>lightbulb_outline</Icon>
-									</ListItemIcon>
-
-									<ListItemText primary='Brightness' secondary={
-										<Slider
-											min={ 0 }
-											max={ 20 }
-											defaultValue={ 5 }
-											trackStyle={{ backgroundColor: theme.palette.primary.main }}
-											handleStyle={{
-												borderColor: theme.palette.primary.main,
-												backgroundColor: theme.palette.primary.main,
-											}}
-											railStyle={{ backgroundColor: 'lightgray' }}
-										/>
-									}
-									/>
-
-								</ListItem>
-								<ListItem
-									button
-									aria-haspopup='true'
-									aria-controls='lorem-ipsum'
-									aria-label='Lorem ipsum'
-									// onClick={this.handleClickListItem}
-								>
-									<ListItemIcon>
-										<Icon>color_lens</Icon>
-									</ListItemIcon>
-
-									<ListItemText primary='Colors' secondary='Choose colors of your lights' />
-								</ListItem>
-								<ListItem divider>
-									<ListItemIcon>
-										<Icon>brightness_2</Icon>
-									</ListItemIcon>
-
-									<ListItemText primary='Night mode' />
-
-									<ListItemSecondaryAction>
-										<Switch
-											checked={this.state.checkedNightMode}
-											onChange={this.handleChange('checkedNightMode')}
-										/>
-									</ListItemSecondaryAction>
-								</ListItem>
-								{/*<ListItem
-									button
-									aria-haspopup='true'
-									aria-controls='lorem-ipsum'
-									aria-label='Lorem ipsum'
-									// onClick={this.handleClickListItem}
-								>
-									<ListItemIcon>
-										<Icon>lightbulb_outline</Icon>
-									</ListItemIcon>
-
-									<ListItemText disableTypography primary='Volume' secondary='Lorem ipsum'/>
-								</ListItem>*/}
-
-								<Typography className={ classes.controlsTitle } type='subheading'>
-									Temperature
-								</Typography>
-
-								<ListItem button onClick={ this.handleChange('checkedTemperature') } > {/*TODO: fix click*/}
-									<ListItemIcon>
-										<Icon>ac_unit</Icon>
-									</ListItemIcon>
-
-									<ListItemText primary='Air conditioning' />
-
-									<ListItemSecondaryAction>
-										<Switch
-											checked={ this.state.checkedTemperature }
-											onChange={ this.handleChange('checkedTemperature') }
-										/>
-									</ListItemSecondaryAction>
-								</ListItem>
-								<ListItem
-									button
-									divider
-									/*aria-haspopup='true'
-									aria-controls='set-temperature'
-									aria-label='Set temperature'*/
-								>
-									<ListItemIcon>
-										<Icon>touch_app</Icon>
-									</ListItemIcon>
-
-									<ListItemText primary='Set temperature' secondary='14 degrees' /> {/*TODO: insert pop-up menu*/}
-								</ListItem>
-
-								<Typography className={ classes.controlsTitle } type='subheading'>
-									Appliances
-								</Typography>
-
-								<ListItem>
-									<ListItemIcon>
-										<Icon>info</Icon>
-									</ListItemIcon>
-
-									<ListItemText primary='Lorem ipsum' />
-
-									<ListItemSecondaryAction>
-										<Switch
-											checked={this.state.checkedTemperature}
-											onChange={this.handleChange('checkedTemperature')}
-										/>
-									</ListItemSecondaryAction>
-								</ListItem>
-								<ListItem
-									button
-									aria-haspopup='true'
-									aria-controls='lorem-ipsum'
-									aria-label='Lorem ipsum'
-									// onClick={this.handleClickListItem}
-								>
-									<ListItemIcon>
-										<Icon>info</Icon>
-									</ListItemIcon>
-
-									<ListItemText primary='Lorem ipsum' secondary='Lorem ipsum' />
-								</ListItem>
-							</List>
-						
-						</div>
-                    </div>
+					<RoomNavigation />
                 </div>
             </div>
         );
--- src/views/controls/RoomNavigation.js
+++ src/views/controls/RoomNavigation.js
@@ -4,6 +4,8 @@ import { withStyles } from 'material-ui/styles';
 import grey from 'material-ui/colors/grey';
 import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 
+import LivingRoom from './rooms/LivingRoom';
+
 const styles = theme => ({
     navContainer: {
         backgroundColor: grey[200],
@@ -37,35 +39,38 @@ class RoomNavigation extends Component {
         const { value } = this.state;
 
         return (
-            <div>
-				<List component='nav' className={ classes.navContainer }>
-					<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(0) }>
-						<ListItemText primary='All' classes={{ primary: value === 0 ? classes.checked : 'unchecked' }} />
-					</ListItem>
-					<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(1) }>
-						<ListItemText primary='Living room' classes={{ primary: value === 1 ? classes.checked : 'unchecked' }} />
-					</ListItem>
-					<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(2) }>
-						<ListItemText primary='Dinner room' classes={{ primary: value === 2 ? classes.checked : 'unchecked' }} />
-					</ListItem>
-					<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(3) }>
-						<ListItemText primary='Bed room' classes={{ primary: value === 3 ? classes.checked : 'unchecked' }} />
-					</ListItem>
-					<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(4) }>
-						<ListItemText primary='Bathroom' classes={{ primary: value === 4 ? classes.checked : 'unchecked' }} />
-					</ListItem>
-					<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(5) }>
-						<ListItemText primary='Hallway' classes={{ primary: value === 5 ? classes.checked : 'unchecked' }} />
-					</ListItem>
-					<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(6) }>
-						<ListItemText primary='Outdoor' classes={{ primary: value === 6 ? classes.checked : 'unchecked' }} />
-					</ListItem>
-				</List>
-
-				{ value === 0 && <div>asdf</div> }
-				{ value === 1 && <div>asdf2</div> }
-				{ value === 2 && <div>asdf3</div> }
-				{ value === 3 && <div>asdf4</div> }
+			<div className='row'>
+				<div className='col-xs-5'>
+					<List component='nav' className={ classes.navContainer }>
+						<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(0) }>
+							<ListItemText primary='All' classes={{ primary: value === 0 ? classes.checked : 'unchecked' }} />
+						</ListItem>
+						<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(1) }>
+							<ListItemText primary='Living room' classes={{ primary: value === 1 ? classes.checked : 'unchecked' }} />
+						</ListItem>
+						<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(2) }>
+							<ListItemText primary='Dinner room' classes={{ primary: value === 2 ? classes.checked : 'unchecked' }} />
+						</ListItem>
+						<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(3) }>
+							<ListItemText primary='Bed room' classes={{ primary: value === 3 ? classes.checked : 'unchecked' }} />
+						</ListItem>
+						<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(4) }>
+							<ListItemText primary='Bathroom' classes={{ primary: value === 4 ? classes.checked : 'unchecked' }} />
+						</ListItem>
+						<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(5) }>
+							<ListItemText primary='Hallway' classes={{ primary: value === 5 ? classes.checked : 'unchecked' }} />
+						</ListItem>
+						<ListItem className={ classes.listItem } button onClick={ () => this.handleClick(6) }>
+							<ListItemText primary='Outdoor' classes={{ primary: value === 6 ? classes.checked : 'unchecked' }} />
+						</ListItem>
+					</List>
+				</div>
+				<div className='col-xs-7'>
+					{ value === 0 && <LivingRoom /> }
+					{ value === 1 && <LivingRoom /> }
+					{ value === 2 && <LivingRoom /> }
+					{ value === 3 && <LivingRoom /> }
+				</div>
             </div>
         );
     }
