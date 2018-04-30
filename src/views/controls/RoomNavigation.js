e482eef5a7515a80cf7bd121e7f8f40b54b7d536
Jordy-A <jordyalblas@live.nl>
2018-04-30 21:58:52 +0200

    New Controls layout

--- src/views/controls/RoomNavigation.js
+++ src/views/controls/RoomNavigation.js
@@ -3,6 +3,17 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import grey from 'material-ui/colors/grey';
 import List, { ListItem, ListItemText } from 'material-ui/List';
+import Icon from 'material-ui/Icon';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  // DialogContentText,
+  // DialogTitle,
+  // withMobileDialog,
+} from 'material-ui/Dialog';
+import Button from 'material-ui/Button';
 
 import All from './rooms/All';
 import LivingRoom from './rooms/LivingRoom';
@@ -10,18 +21,20 @@ import DinnerRoom from './rooms/DinnerRoom';
 
 const styles = theme => ({
     subNavContainer: {
-		width: '30%',
-		maxWidth: 175,
-		float: 'left',
 		whiteSpace: 'nowrap',
     },
-    subNavList: {
+    subNavItem: {
+		marginTop: 10,
+		marginBottom: 10,
+		display: 'table',
+    },
+    subNavItemPaper: {
         backgroundColor: grey[200],
-    },
-    subNavContent: {
-		width: '70%',
-		float: 'left',
-    },
+		height: 120,
+		cursor: 'pointer',
+		display: 'table-cell',
+		verticalAlign: 'middle',
+	},
     checked: {
         color: theme.palette.primary.main,
     },
@@ -32,13 +45,14 @@ const styles = theme => ({
 });
 
 let rooms = [
-    { id: 1, value: 'All', component: <All key={ 1 } />, },
-    { id: 2, value: 'Living Room', component: <LivingRoom key={ 2 } />, },
-    { id: 3, value: 'Dinner Room', component: <DinnerRoom key={ 3 } />, },
-    { id: 4, value: 'Bedroom', component: '', },
-    { id: 5, value: 'Bathroom', component: '', },
-    { id: 6, value: 'Hallway', component: '', },
-    { id: 7, value: 'Outdoor', component: '', },
+    { id: 1, value: 'All', icon: 'check_circle', component: <All key={ 1 } />, },
+    { id: 2, value: 'Living Room', icon: 'event_seat', component: <LivingRoom key={ 2 } />, },
+    { id: 3, value: 'Dinner Room', icon: 'free_breakfast', component: <DinnerRoom key={ 3 } />, },
+    { id: 4, value: 'Bedroom', icon: 'hotel', component: '', },
+    { id: 5, value: 'Bathroom', icon: 'hot_tub', component: '', },
+    { id: 6, value: 'Hallway', icon: 'transfer_within_a_station', component: '', },
+	{ id: 7, value: 'Kitchen', icon: 'local_dining', component: '', },
+    { id: 8, value: 'Outdoor', icon: 'local_florist', component: '', },
 ];
 
 class RoomNavigation extends Component {
@@ -46,6 +60,7 @@ class RoomNavigation extends Component {
         super(props);
         this.state = {
             value: 1,
+			openRoomPopup: false,
         };
     }
 
@@ -54,6 +69,14 @@ class RoomNavigation extends Component {
             value: id,
         });
     };
+	
+	handleRoomPopupOpen = () => {
+		this.setState({ openRoomPopup: true });
+	};
+
+	handleRoomPopupClose = () => {
+		this.setState({ openRoomPopup: false });
+	};
 
     render() {
         const { classes } = this.props;
@@ -61,27 +84,42 @@ class RoomNavigation extends Component {
 
         return (
 			<div>
-				<div className={ classes.subNavContainer }>
-					<List component='nav' className={ classes.subNavList }>
+				<div className="row">
+					<div className={ classes.subNavContainer }>
 						{ rooms.map(data => {
 							return (
-								<ListItem key= { data.id } classes={{
-										gutters: classes.listItemGutters,
-									}} button onClick={ () => this.handleClick(data.id) }>
-									<ListItemText primary={ data.value } classes={{ primary: value === data.id ? classes.checked : 'unchecked' }} />
-								</ListItem>
+								<div className={[classes.subNavItem, "col-xs-4"].join(' ')}>
+									<Paper className={classes.subNavItemPaper} elevation={2} onClick={ () => { this.handleClick(data.id);this.handleRoomPopupOpen(); } }>
+										<Icon style={{ fontSize: 30 }}>{ data.icon }</Icon>
+										<Typography component="p">
+											{ data.value }
+										</Typography>
+									</Paper>
+								</div>
 							);
 						}) }
-					</List>
+					</div>
 				</div>
-				<div className={ classes.subNavContent }>
-					{ rooms.map(data => {
-						return (
-							value === data.id && data.component
-						);
-					}) }
-				</div>
-            </div>
+				
+				
+				<Dialog
+					open={this.state.openRoomPopup}
+					onClose={this.handleRoomPopupClose}
+				>
+					<DialogContent>
+						{ rooms.map(data => {
+							return (
+								value === data.id && data.component
+							);
+						}) }
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={this.handleRoomPopupClose} color="secondary">
+							Close
+						</Button>
+					</DialogActions>
+				</Dialog>
+			</div>
         );
     }
 }
