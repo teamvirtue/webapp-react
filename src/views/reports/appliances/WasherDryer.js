0f3bea4f0e80ea0ecb4e5d6bf17d034b283a0e2b
Unknown <mataharata@gmail.com>
2018-05-08 15:31:00 +0200

    Axios requests

--- src/views/reports/appliances/WasherDryer.js
+++ src/views/reports/appliances/WasherDryer.js
@@ -10,6 +10,7 @@ import Axios from 'axios';
 
 class WasherDryer extends Component {
     //http://localhost:8000/api-auth/login/?next=/appliance/auth/
+
     constructor() {
       super();
       this.state = {
@@ -46,30 +47,52 @@ class WasherDryer extends Component {
     //   .catch((error) => {
     //     console.log(error);
     //   });
-      Axios.get("http://localhost:8000/appliance/1/")
+    var token = "";
+      // Axios.get("http://localhost:8000/users/")
+      // .then((response) => {
+      //   console.log(response);
+      //   return response.data;
+      // });
+      // })
+      Axios.post("http://localhost:8000/api/auth/token/", "username=matahara&password=katinar4e ")
       .then((response) => {
-        console.log(response);
-        return response.data;
-      }).then((data)=>{
-        this.setState({
-           id: data.appliance_id,
-           status : data.appliance_status,
-           type_id : data.appliance_type_id,
-           created : data.created,
-           flat_id : data.flat_id,
-           energy_consumed : data.last_appliance_energy_consumed,
-           reading_time : data.last_reading_time,
-           water_reading : data.last_water_reading,
-           modified : data.modified,
-           room_id : data.room_id,
-           url : data.url,
-        })
-        console.log(this.state);
-      })
-      .catch((error) => {
-        console.log(error);
+          var data = response.data;
+          token = data.token;
+          console.log(token);
+          const instance = Axios.create({
+            baseURL: 'http://localhost:8000',
+            headers: {'Authorization': 'JWT ' + token},
+          });
+          console.log(instance.defaults);
+          instance.get('/users', {
+            timeout: 5000
+          }).then((response) => {
+            console.log(response.data);
+            //return response.data;
+          });
+          instance.get('/appliance/1/', {
+            timeout: 5000
+          }).then((response) => {
+            console.log(response.data);
+            this.setState({
+               id : response.data.appliance_id,
+               status : response.data.appliance_status,
+               type_id : response.data.appliance_type_id,
+               created : response.data.created,
+               flat_id : response.data.flat_id,
+               energy_consumed : response.data.last_appliance_energy_consumed,
+               reading_time : response.data.last_reading_time,
+               water_reading : response.data.last_water_reading,
+               modified : response.data.modified,
+               room_id : response.data.room_id,
+               url : response.data.url,
+            })
+            console.log(this.state);
+            return this.state;
+          });
       });
     }
+
 
 
 
@@ -80,7 +103,10 @@ class WasherDryer extends Component {
         <div>
 				<List>
             <Typography className={ classes.controlsTitle } type='subheading'>
-  						{this.state.url}
+            {this.state.id}
+            {this.state.status}
+            {this.state.type_id}
+  					{this.state.flat_id}
   					</Typography>
 					<Typography className={ classes.controlsTitle } type='subheading'>
 						Chart
