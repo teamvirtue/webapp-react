3dcf55a4cbfc0c654a0a248f9b13d42b8bee8425
Jordy-A <jordyalblas@live.nl>
2018-07-17 21:29:10 +0200

    Added Linq status dialog

--- src/ImageCircle.js
+++ src/ImageCircle.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import grey from '@material-ui/core/colors/grey';
+import { withStyles } from '@material-ui/core/styles';	
+import Dialog from '@material-ui/core/Dialog';
 import ThreeEntryPoint from './threejs/ThreeEntryPoint';
 
 const styles = theme => ({
-	linqStatus: {
-		display: 'inline-block',
-		width: '22vw',
-		height: '22vw',
-		top: '50%',
-		margin: '-11vw 0 0 -11vw',
-	},
-
 	circle: {
 		position: 'absolute',
 		height: '94%',
@@ -22,7 +14,7 @@ const styles = theme => ({
 		animation: 'borderGradient 3s 1000ms infinite alternate',
 		borderRadius: '50%',
 	},
-	circleBorder: {
+	circleBorder: {cursor: 'pointer',
 		position: 'absolute',
 		height: '100%',
 		width: '100%',
@@ -47,18 +39,37 @@ class ImageCircle extends Component {
     componentDidMount() {
         ThreeEntryPoint(this.threeRootElement);
     }
+	
+	state = {
+		open: false,
+	};
+
+	handleClickOpen = () => {
+		this.setState({ open: true });
+	};
+
+	handleClose = () => {
+		this.setState({ open: false });
+	};
 
     render() {
 		const { classes, gutterTop } = this.props;
 
         return (
 			<div>
-				<div className='linqStatusCircle'>
+				<div className='linqStatusCircle' onClick={this.handleClickOpen}>
 					<div className={ classes.circle } ref={ element => this.threeRootElement = element } />
 					<svg className={ classes.circleBorder } version="1.1" id="L1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 100 100">
 						<circle cx="50" cy="50" r="48"/>
 					</svg>
 				</div>
+				
+				<Dialog
+					open={this.state.open}
+					onClose={this.handleClose}
+				>
+					<div>Tips about LINQ</div>
+				</Dialog>
 			</div>
         );
     }
