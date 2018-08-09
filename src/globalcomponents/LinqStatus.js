82258e26e9b1d4ce67b9ef92a281dc63a9540cf1
Jordy-A <jordyalblas@live.nl>
2018-08-09 23:44:08 +0200

    Fixed circle border animation and dialog

--- src/globalcomponents/LinqStatus.js
+++ src/globalcomponents/LinqStatus.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';	
 import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import withMobileDialog from '@material-ui/core/withMobileDialog';
+import Button from '@material-ui/core/Button';
+import Grow from '@material-ui/core/Grow';
 import Scene from '../threejs/Scene';
 // import ThreeEntryPoint from '../threejs/ThreeEntryPoint';
 
@@ -11,15 +16,6 @@ const styles = theme => ({
         width: '100%',
 		backgroundColor: 'blue',*/
 	},
-    /*linqStatusCircle: { // TODO: combine with 'circle'
-        position: 'fixed',
-        display: 'inline-block',
-        width: '22vw',
-        height: '22vw',
-        top: '50%',
-        margin: '-11vw 0 0 -11vw',
-        transition: 'all 200ms ease-out',
-    },*/
 	circle: {
 		position: 'absolute',
 		height: '100%',
@@ -43,10 +39,21 @@ const styles = theme => ({
 		strokeDasharray: '12',
 		strokeDashoffset: '0',
 		strokeLinecap: 'square',
-		animation: 'startProgress 1500ms ease-out, continueProgress 12s 1500ms linear infinite',
+		animation: 'startProgress 1500ms ease-out, continueProgress 15s 1500ms linear infinite',
 		animationFillMode: 'forwards',
+		pointerEvents: 'none',
+	},
+	dialogSmall: {
+		minWidth: 350,
+	},
+	dialogFull: {
+		minWidth: 0,
 	},
 });
+
+function Transition(props) {
+	return <Grow {...props} />;
+}
 
 class LinqStatus extends Component {
     /*constructor(props) {
@@ -61,35 +68,47 @@ class LinqStatus extends Component {
 		open: false,
 	};
 
-	/*handleClickOpen = () => {
+	handleClickOpen = () => {
 		this.setState({ open: true });
-	};*/
+	};
 
 	handleClose = () => {
 		this.setState({ open: false });
 	};
 
     render() {
+		const { fullScreen } = this.props;
 		const { classes } = this.props;
 
         return (
 			<div className={ classes.root }>
 				<div className='linqStatusCircle' onClick={ this.handleClickOpen }>
-				{/*<div className={ classes.linqStatusCircle } onClick={ this.handleClickOpen }>*/}
-					{/*<div className={ classes.circle } ref={ element => this.threeRootElement = element } />*/}
 					<div className={ classes.circle }>
                         <Scene />
                     </div>
-					{/*<svg className={ classes.circleBorder } version='1.1' id='L1' xmlns='http://www.w3.org/2000/svg' x='0px' y='0px' viewBox='0 0 100 100'>
-						<circle cx='50' cy='50' r='46'/>
-					</svg>*/}
+					<svg className={ classes.circleBorder } version='1.1' id='L1' xmlns='http://www.w3.org/2000/svg' x='0px' y='0px' viewBox='0 0 100 100'>
+						<circle cx='50' cy='50' r='48'/>
+					</svg>
 				</div>
 				
 				<Dialog
+					fullScreen={ fullScreen }
 					open={ this.state.open }
+					TransitionComponent={Transition}
 					onClose={ this.handleClose }
+					classes={{
+						paperWidthSm: classes.dialogSmall,
+						paperFullScreen: classes.dialogFull,
+					}}
 				>
-					<div>Tips about LINQ</div>
+					<DialogContent>
+						<div>Tips about LINQ</div>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={ this.handleClose } color='primary'>
+							Close
+						</Button>
+					</DialogActions>
 				</Dialog>
 			</div>
         );
@@ -97,8 +116,9 @@ class LinqStatus extends Component {
 }
 
 LinqStatus.propTypes = {
+	fullScreen: PropTypes.bool.isRequired,
     classes: PropTypes.object.isRequired,
     //src: PropTypes.string.isRequired,
 };
 
-export default withStyles(styles)(LinqStatus);
+export default withStyles(styles)(withMobileDialog()(LinqStatus));
